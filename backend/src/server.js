bdc49e63853c0992dc421d38d464fb8550a42750
Rahul Kumar Singh <contactrahulkrsingh@gmail.com>
2025-03-30 14:07:50 +0530

    Added port

--- backend/src/server.js
+++ backend/src/server.js
@@ -27,5 +27,5 @@ app.use((err, req, res, next) => {
 
 const PORT = config.PORT;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT || 5000}`);
 }); 
